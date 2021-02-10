9c715497cadef70a146c904df7e062d1d12559da
Michael Swan <RMichaelSwan@gmail.com>
2021-02-09 22:42:48 -0800

    Robust detection and capture of partipant windows.

--- capture.py
+++ capture.py
@@ -7,82 +7,277 @@
     Wnck API Reference: https://lazka.github.io/pgi-docs/Wnck-3.0/classes/Screen.html
     Gdk API Reference: https://lazka.github.io/pgi-docs/Gdk-3.0/classes/Screen.html
 
-    TODO: Process in OpenCV to get separate squares for each box in the window
-    TODO: Allow user to select desired window to record
+    TODO: Get audio stream
+    TODO: Move CV box acquisition code to proper functions/modules
+    TODO: provide video stream
 """
+
+import numpy as np
+import cv2
 
 import time
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Wnck', '3.0')
 gi.require_version('Gdk', '3.0')
-from gi.repository import Gtk, Gdk, Wnck
+from gi.repository import Gtk, Gdk, Wnck, GdkPixbuf
 
-Gtk.init([])  # necessary only if not using a Gtk.main() loop
-screen = Wnck.Screen.get_default()
-screen.force_update()  # recommended per Wnck documentation
+def windows2png():
+    """Capture all windows and save to file
+    """
+    screen = Gdk.Screen.get_default()
+    for i, window in enumerate(screen.get_window_stack()):
+        pb = Gdk.pixbuf_get_from_window(window, *window.get_geometry())
+        pb.savev("{}.png".format(i), "png", (), ())
 
-# loop all windows
-found_win = False
-for i, window in enumerate(screen.get_windows()):
-    name = window.get_name()
-    if (name == "Zoom Meeting"):
-        # Activate desired window  and wait for it to come up since there seems 
-        # to be no way to tell Gdk which window to use directly.
-        found_win = True
-        window.activate(0)
-        time.sleep(0.1)
-        break
-    # print(i, name)
+def array_from_pixbuf(p):
+    """Convert from GdkPixbuf to numpy array"
 
-if (not found_win):
-    "Unable to find, activate, and record desired window."
+    Args:
+        p (GdkPixbuf): The GdkPixbuf provided from some window handle
 
-screen = Gdk.Screen.get_default()
-window = screen.get_active_window()
-pb = Gdk.pixbuf_get_from_window(window, *window.get_geometry())
-pb.savev("{}.png".format(0), "png", (), ())
+    Returns:
+        ndarray: The numpy array arranged for the pixels in height, width, RGBA order
+    """
+    w,h,c,r=(p.get_width(), p.get_height(), p.get_n_channels(), p.get_rowstride())
+    assert p.get_colorspace() == GdkPixbuf.Colorspace.RGB
+    assert p.get_bits_per_sample() == 8
+    if  p.get_has_alpha():
+        assert c == 4
+    else:
+        assert c == 3
+    assert r >= w * c
+    a=np.frombuffer(p.get_pixels(),dtype=np.uint8)
+    if a.shape[0] == w*c*h:
+        return a.reshape( (h, w, c), order = 'C' )
+    else:
+        b=np.zeros((h,w*c),'uint8')
+        for j in range(h):
+            b[j,:]=a[r*j:r*j+w*c]
+        return b.reshape( (h, w, c) )
 
-# --FPS Test--
-# TODO: move this test to a proper test case
-i = 1
-frames = 100
-start = time.time()
-while (i <= frames):
-  pb = Gdk.pixbuf_get_from_window(window, *window.get_geometry())
-#   pb.savev("{}.png".format(i), "png", (), ())
-  i += 1
-print("--- achieved %s fps ---" % (frames / (time.time() - start)))
-# --End Test--
+def array_to_cv2(pix_array, colorspace=cv2.COLOR_RGB2BGR):
+    return cv2.cvtColor(pix_array, colorspace)
 
-# clean up Wnck (saves resources, check documentation)
-window = None
-screen = None
-Wnck.shutdown()
+class WindowCapture():
+    def __init__(self, window_title="Zoom Meeting", activation_delay = 0.1):
+        """Captures from GUI window of specified title. Serves as a handle on 
+            that window as long as it is still available. If the window is 
+            covered up by another window after init finishes, the window can
+            still be captured from.
+
+        Args:
+            window_title (str, optional): Name of the window to capture from. 
+                Defaults to "Zoom Meeting".
+            activation_delay (float, optional): How long in seconds to wait for
+                a window to be activated so that the correct handle will be 
+                acquired. If the wrong window is captured, increase this number.
+        """
+        self.window, self.window_title, self.activation_delay = None, None, None
+        Gtk.init([])  # necessary since we're not using a Gtk.main() loop
+        self.set_window(window_title=window_title,activation_delay=activation_delay)
+
+    def _get_active_window_(self):
+        """Gets a handle on the active Gdk window.
+
+        Returns:
+            Gdk.Window: The window handle.
+        """
+        screen = Gdk.Screen.get_default()
+        return screen.get_active_window()
+
+    def _activate_window_(self):
+        """This function uses Wnck to set a window as the active/focused window on 
+            the user's screen. It is especially useful when combined with Gdk to 
+            get active window pixels. Note: Gdk has no direct way of accessing a 
+            window by title.
+
+        Returns:
+            bool: True if window found and activated, else False
+        """
+        screen = Wnck.Screen.get_default()
+        screen.force_update()  # recommended per Wnck documentation
+        found_win = False
+        for window in screen.get_windows():
+            name = window.get_name()
+            if (name == self.window_title):
+                found_win = True
+                window.activate(0)
+                time.sleep(self.activation_delay)
+
+                ##should be the proper solution but Wnck/Pygi is kinda trash; 
+                ##the active boolean never returns true and proper activate times fail.
+                # window.activate(int(time.time()))
+                # while(not window.is_active()):
+                #     time.sleep(self.activation_delay)
+                break
+
+        # clean up Wnck (saves resources, check documentation)
+        window = None
+        screen = None
+        Wnck.shutdown()
+        return found_win
+    
+    def set_window(self, window_title, activation_delay = 0.1):
+        """Set the window we are going to capture from
+
+        Args:
+            window_title ([type]): [description]
+            activation_delay (float, optional): How long in seconds to wait for
+                a window to be activated so that the correct handle will be 
+                acquired. Some computers may take longer to do this. Defaults to 0.1.
+                NOTE: This delay is a workaround for Wnck's inability to get window state.
+
+        Raises:
+            ValueError: [description]
+        """
+        self.window_title = window_title
+        self.activation_delay = activation_delay
+        if (self._activate_window_()):
+            self.window = self._get_active_window_()
+        else:
+            raise ValueError(f'Unable to get/find window with title "{self.window_title}" for capture.')
 
 
-# --capture all windows--
-# screen = Gdk.Screen.get_default()
-# for i, window in enumerate(screen.get_window_stack()):
-    # print(i, window.list_properties())
-    # print(window.get_state())
-    # if (window.get_state() == Gdk.WindowState.FOCUSED):
-    # pb = Gdk.pixbuf_get_from_window(window, *window.get_geometry())
-    # pb.savev("{}.png".format(i), "png", (), ())
+    def test_fps(self, frames = 100):
+        """Gives an estimate of the frames per second capture rate
+        Args:
+            frames (int, optional): Number of frames to aquire for test. Defaults to 100.
+        Returns:
+            float: The determined FPS for capture.
+        """
+        i = 1
+        start = time.time()
+        while (i <= frames):
+            pb = Gdk.pixbuf_get_from_window(self.window, *self.window.get_geometry())
+            i += 1
+        fps = (frames / (time.time() - start))
+        print("--- achieved %s fps ---" % fps)
+        return fps
+
+    def get_cv_img(self):
+        """Get a new frame from window in OpenCV format.
+        Returns:
+            cv::_OutputArray: Most recent window frame in cv output format.
+        """
+        return cv2.cvtColor(self.get_numpy_img(), cv2.COLOR_RGB2BGR) # cv2 works in BGR colorspace
+
+    def get_numpy_img(self):
+        """Get a new frame from window as a Numpy array in RGBA format
+        Returns:
+            ndarray: (Height, Width, Channels) shaped array
+        """
+        pb = Gdk.pixbuf_get_from_window(self.window, *self.window.get_geometry())
+        return array_from_pixbuf(pb)
+
+
+    def save_window_png(self, filename = "0"):
+        """Save current window frame to png
+        Args:
+            filename (str, optional): Filename to save as (without extension). Defaults to "0".
+        """
+        pb = Gdk.pixbuf_get_from_window(self.window, *self.window.get_geometry())
+        pb.savev(f"{filename}.png", "png", (), ())
 
 
 
+class ShapeDetector:
+	def __init__(self):
+		pass
+	def detect(self, c):
+		# initialize the shape name and approximate the contour
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+		# if the shape is a triangle, it will have 3 vertices
+		if len(approx) == 3:
+			shape = "triangle"
+		# if the shape has 4 vertices, it is either a square or
+		# a rectangle
+		elif len(approx) == 4:
+			# compute the bounding box of the contour and use the
+			# bounding box to compute the aspect ratio
+			(x, y, w, h) = cv2.boundingRect(approx)
+			ar = w / float(h)
+			# a square will have an aspect ratio that is approximately
+			# equal to one, otherwise, the shape is a rectangle
+			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+		# if the shape is a pentagon, it will have 5 vertices
+		elif len(approx) == 5:
+			shape = "pentagon"
+		# otherwise, we assume the shape is a circle
+		else:
+			shape = "circle"
+		# return the name of the shape
+		return shape
+    
+def main():
+    gallery_color = np.array([26,26,26,255]) #zoom gallery color in RGB
+    active_color = np.array([35,217,89,255]) #zoom gallery color in RGB
 
-#X11 attempt (had issues with screen capture, names work though)
-# import Xlib.display
+    x = WindowCapture()
+    img_arr = x.get_numpy_img()
+    print(img_arr.shape)
+    img_arr = img_arr[0:int(img_arr.shape[0]*0.89),0:int(img_arr.shape[1]*0.99),...] #crop out extra bars around window edges
+    print(img_arr.shape)
+    img = array_to_cv2(img_arr,cv2.COLOR_RGB2BGR)
+    masked = img_arr.copy()
+    
+    gal_mask = (img_arr == gallery_color).all(-1)
+    
+    masked[gal_mask] = 255
+    masked[np.logical_not(gal_mask)] = 0
+    
+    ##We can find the active partipant using the color...
+    # active_mask = (img_arr == active_color).all(-1)
+    # masked[...,:4][active_mask] = [0,0,0,0]
+    
+    #reference: https://stackoverflow.com/a/11427501/5715374
+    gray = array_to_cv2(masked,cv2.COLOR_RGB2GRAY)
+    _,thresh = cv2.threshold(gray,127,255,1)
+    contours,_ = cv2.findContours(thresh,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours,key=cv2.contourArea,reverse=True)
+    rects = [] # a list of found rects in descending (greatest first) area order
+    
+    #find the interesting rectangles
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        if len(approx)==4: #rectangles have 4 sides
+            area = cv2.contourArea(cnt)
+            # print(cv2.contourArea(cnt))
+            if area < 100: #ignore tiny rectangles
+                continue
+            _,_,w,h = cv2.boundingRect(cnt)
+            if (w/h > 5): #bad aspect ratio
+                # print(f"rejected aspect ratio of {w/h}")
+                continue
+            # print(f"Aspect ratio = {w/h}")
+            rects.append(cnt)
 
-# screen = Xlib.display.Display().screen()
-# root_win = screen.root
+    first = True
+    color = (0,0,255)
+    display = False
+    participants = []
+    for rect in rects:
+        x,y,w,h = cv2.boundingRect(rect)
+        participants.append(img[y:y+h, x:x+w]) #crop participant image to bounding box.
+        #the first participant has the largest area and is thus most likely the active participant
+        if first: 
+            first = False
+            color = (0,255,0)
+        else:
+            color = (0,0,255)
+        if display:
+            cv2.drawContours(img,[rect],0,color,2)
+    print(f"Detected {len(rects)} participants.")
 
-# savew = None
-# window_names = []
-# for window in root_win.query_tree()._data['children']:
-#     window_name = window.get_wm_name()
-#     window_names.append(window_name)
+    cv2.imshow("Full", gray)
+    for i, part in enumerate(participants):
+        cv2.imshow(f"Participant {i}",part)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    x = None
 
-# print(window_names)
+if __name__ == "__main__":
+    main()
