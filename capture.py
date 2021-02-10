be3bce2b9f8c2ecb52adad152d01d8aafcf17497
Michael Swan <RMichaelSwan@gmail.com>
2021-02-10 00:08:18 -0800

    Refactored detector code slightly and improved documentation.

--- capture.py
+++ capture.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
 """
-    A script to capture the screen from specific Gtk windows using Wnck.
-    Assuming the window is acquired correctly, it does not have to be on top once
-    session is started (minimization may cause a problem though).
-    Wnck API Reference: https://lazka.github.io/pgi-docs/Wnck-3.0/classes/Screen.html
-    Gdk API Reference: https://lazka.github.io/pgi-docs/Gdk-3.0/classes/Screen.html
+    A script to capture the speaker audio and screen
 
     TODO: Get audio stream
-    TODO: Move CV box acquisition code to proper functions/modules
     TODO: provide video stream
 """
 
@@ -64,7 +59,12 @@ class WindowCapture():
         """Captures from GUI window of specified title. Serves as a handle on 
             that window as long as it is still available. If the window is 
             covered up by another window after init finishes, the window can
-            still be captured from.
+            still be captured from. 
+            
+            Thie captures from specific Gtk windows using Wnck, which means
+            it is likely only compatible on Linux gui environments.
+            Wnck API Reference: https://lazka.github.io/pgi-docs/Wnck-3.0/classes/Screen.html
+            Gdk API Reference: https://lazka.github.io/pgi-docs/Gdk-3.0/classes/Screen.html
 
         Args:
             window_title (str, optional): Name of the window to capture from. 
@@ -181,97 +181,147 @@ class WindowCapture():
 
 
 
-class ShapeDetector:
-	def __init__(self):
-		pass
-	def detect(self, c):
-		# initialize the shape name and approximate the contour
-		shape = "unidentified"
-		peri = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-		# if the shape is a triangle, it will have 3 vertices
-		if len(approx) == 3:
-			shape = "triangle"
-		# if the shape has 4 vertices, it is either a square or
-		# a rectangle
-		elif len(approx) == 4:
-			# compute the bounding box of the contour and use the
-			# bounding box to compute the aspect ratio
-			(x, y, w, h) = cv2.boundingRect(approx)
-			ar = w / float(h)
-			# a square will have an aspect ratio that is approximately
-			# equal to one, otherwise, the shape is a rectangle
-			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
-		# if the shape is a pentagon, it will have 5 vertices
-		elif len(approx) == 5:
-			shape = "pentagon"
-		# otherwise, we assume the shape is a circle
-		else:
-			shape = "circle"
-		# return the name of the shape
-		return shape
-    
+class PartcipantDetector():
+    def __init__(self,image, epsilon = 0.01, detect_rate = 1, display_box = False, debug = False,
+        gallery_color_rgba = np.array([26,26,26,255]), active_color_rgba = np.array([35,217,89,255]), 
+        crop_percentX = 0.99, crop_percentY = 0.89, aspectRatio = 1.77):        
+        """Detects participants in a video call and provides frames/video from 
+            that call. Current model assumes use of Zoom in gallery mode.
+        
+        #TODO separate detection code from box detection code (make use of detect_rate)
+        #TODO optimize this for frequent function calls/video streaming. 
+            Possible options for optimization include: scaling down image before
+            running box detection, only running detection when major image changes
+            occur.
+
+        Args:
+            image (ndarray): The RGBA image to work off of.
+                The array values are expected to be readonly.
+            epsilon (float, optional): How sensitive the box detector is.
+                A higher value results in boxes with imperfections being detected
+                more often. Defaults to 0.1.
+            detect_rate (int, optional): How often in Hz to check participant 
+                window positions and active participant. Defaults to 1 Hz.
+            display_box (bool, optional): Whether to display detection boxes 
+                around frames. Defaults to False.
+            debug (bool, optional): Activates a number of debug outputs 
+            gallery_color_rgba (ndarray, optional): The RGBA color surrounding the participant window. 
+                Defaults to np.array([26,26,26,255]) which is the Zoom default.
+            active_color_rgba (ndarray, optional): The primary color of the box
+                 that highlights the active user.. Defaults to np.array([35,217,89,255]).
+            crop_percentX (float, optional): How much extra content to keep 
+                from image window width-wise; lower percents remove more from 
+                the right side. Defaults to 0.99.
+            crop_percentY (float, optional): How much extra content to keep 
+                from image window height-wise; lower percents remove more from 
+                the bottom side. Defaults to 0.89.
+            aspectRatio (float, optional): The predicted aspect ratio of 
+                participant videos. Used to filter out false positives. Defaults to 1.77.
+        """        
+        self.img_arr = image
+        self.epsilon = epsilon
+        self.detect_rate = detect_rate
+        self.display_box = display_box
+        self.debug = debug
+        self.gallery_color = gallery_color_rgba
+        self.active_color = active_color_rgba
+        self.crop_percentX = crop_percentX
+        self.crop_percentY = crop_percentY
+        self.aspectRatio = aspectRatio
+
+    def detect_new(self,image):
+        self.img_arr = image
+        self.detect()
+
+    def detect(self):
+        #crop out extra bars around window edges
+        self.img_arr = self.img_arr[0:int(self.img_arr.shape[0]*self.crop_percentY),
+                                    0:int(self.img_arr.shape[1]*self.crop_percentX),...] 
+        if self.debug: print(f"Cropped image array shape: {self.img_arr.shape}")
+        img = array_to_cv2(self.img_arr,cv2.COLOR_RGB2BGR)
+        masked = self.img_arr.copy()
+        
+        gal_mask = (self.img_arr == self.gallery_color).all(-1)
+        
+        masked[gal_mask] = 255
+        masked[np.logical_not(gal_mask)] = 0
+        
+        ##We can find the active partipant using the color...
+        # active_mask = (self.img_arr == self.active_color).all(-1)
+        # masked[...,:4][active_mask] = [0,0,0,0]
+        
+        #reference: https://stackoverflow.com/a/11427501/5715374
+        gray = array_to_cv2(masked,cv2.COLOR_RGB2GRAY)
+        if self.debug: cv2.imshow("Full", gray)
+
+        _,thresh = cv2.threshold(gray,127,255,1)
+        contours,_ = cv2.findContours(thresh,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours,key=cv2.contourArea,reverse=True)
+        rects = [] # a list of found rects in descending (greatest first) area order
+        
+        #find the interesting rectangles
+        for cnt in contours:
+            approx = cv2.approxPolyDP(cnt,self.epsilon*cv2.arcLength(cnt,True),True)
+            if len(approx)==4: #rectangles have 4 sides
+                area = cv2.contourArea(cnt)
+                # print(cv2.contourArea(cnt))
+                if area < 100: #ignore tiny rectangles
+                    continue
+                _,_,w,h = cv2.boundingRect(cnt)
+                ar = w/h
+                if (ar > 3*self.aspectRatio or ar < self.aspectRatio/3): #bad aspect ratio
+                    if self.debug: print(f"rejected aspect ratio of {ar}")
+                    continue
+                if self.debug: print(f"Aspect ratio = {ar}")
+                rects.append(cnt)
+
+        first = True
+        color = (0,0,255)
+        display = False
+        participants = []
+        for rect in rects:
+            x,y,w,h = cv2.boundingRect(rect)
+            participants.append(img[y:y+h, x:x+w]) #crop participant image to bounding box.
+            #the first participant has the largest area and is thus most likely the active participant
+            if first: 
+                first = False
+                color = (0,255,0)
+            else:
+                color = (0,0,255)
+            if self.display_box:
+                cv2.drawContours(img,[rect],0,color,2)
+        if self.debug: print(f"Detected {len(rects)} participants.")
+        return participants
+
+def test_pipeline_fps(wc, pd, frames = 100):    
+    """Gives an estimate of the frames per second capture rate
+        for the image capture and box detection pipeline
+    Args:
+        wc (WindowCapture): A handle on the current window capture
+        pd (Participant): A handle on the participant detector
+        frames (int, optional): Number of frames to aquire for test. Defaults to 100.
+    Returns:
+        float: The determined FPS for capture.
+    """
+    i = 1
+
+    start = time.time()
+    while (i <= frames):
+        pd.detect_new(wc.get_numpy_img())
+        i += 1
+    fps = (frames / (time.time() - start))
+    print("--- achieved %s fps ---" % fps)
+    return fps
+
 def main():
-    gallery_color = np.array([26,26,26,255]) #zoom gallery color in RGB
-    active_color = np.array([35,217,89,255]) #zoom gallery color in RGB
 
-    x = WindowCapture()
-    img_arr = x.get_numpy_img()
-    print(img_arr.shape)
-    img_arr = img_arr[0:int(img_arr.shape[0]*0.89),0:int(img_arr.shape[1]*0.99),...] #crop out extra bars around window edges
-    print(img_arr.shape)
-    img = array_to_cv2(img_arr,cv2.COLOR_RGB2BGR)
-    masked = img_arr.copy()
-    
-    gal_mask = (img_arr == gallery_color).all(-1)
-    
-    masked[gal_mask] = 255
-    masked[np.logical_not(gal_mask)] = 0
-    
-    ##We can find the active partipant using the color...
-    # active_mask = (img_arr == active_color).all(-1)
-    # masked[...,:4][active_mask] = [0,0,0,0]
-    
-    #reference: https://stackoverflow.com/a/11427501/5715374
-    gray = array_to_cv2(masked,cv2.COLOR_RGB2GRAY)
-    _,thresh = cv2.threshold(gray,127,255,1)
-    contours,_ = cv2.findContours(thresh,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours,key=cv2.contourArea,reverse=True)
-    rects = [] # a list of found rects in descending (greatest first) area order
-    
-    #find the interesting rectangles
-    for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        if len(approx)==4: #rectangles have 4 sides
-            area = cv2.contourArea(cnt)
-            # print(cv2.contourArea(cnt))
-            if area < 100: #ignore tiny rectangles
-                continue
-            _,_,w,h = cv2.boundingRect(cnt)
-            if (w/h > 5): #bad aspect ratio
-                # print(f"rejected aspect ratio of {w/h}")
-                continue
-            # print(f"Aspect ratio = {w/h}")
-            rects.append(cnt)
+    x = WindowCapture(window_title="Zoom Meeting")
+    # x.test_fps()
+    detector = PartcipantDetector(x.get_numpy_img(), debug=False, display_box=False)
+    participants = detector.detect()
+    # test_pipeline_fps(x,detector)
 
-    first = True
-    color = (0,0,255)
-    display = False
-    participants = []
-    for rect in rects:
-        x,y,w,h = cv2.boundingRect(rect)
-        participants.append(img[y:y+h, x:x+w]) #crop participant image to bounding box.
-        #the first participant has the largest area and is thus most likely the active participant
-        if first: 
-            first = False
-            color = (0,255,0)
-        else:
-            color = (0,0,255)
-        if display:
-            cv2.drawContours(img,[rect],0,color,2)
-    print(f"Detected {len(rects)} participants.")
 
-    cv2.imshow("Full", gray)
     for i, part in enumerate(participants):
         cv2.imshow(f"Participant {i}",part)
     
